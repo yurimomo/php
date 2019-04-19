e7a3590a6d9302d23d08bb8a506c2dde1a90ce47
yuriko <yuri715lily_momo@yahoo.co.jp>
2019-04-19 20:14:13 +0900

    Double_loop

--- index.php
+++ index.php
@@ -1,20 +1,52 @@
 <html>
 <head>
-	<style type="text/css">table{border-collapse: collapse;}</style>
+	<style type="text/css">
+		table{border-collapse: collapse;}
+	</style>
 </head>
+
 <body>
+
+	<!-- trを作成 -->
+	<!--  td8回作成-->
+	<!-- trに戻る -->
  <table border="1">
  <?php
- for ($i = 1; $i < 5; $i ++) {
+ for ($i = 1; $i <= 5; $i ++) {
  	echo "<tr>";
- 	echo "<td>".$i."</td>";
-
- 	for ($w = 1; $w < 5; $w ++) {
- 		echo "<td>".$w."</td>";
+ 	for ($w = 1; $w <= 8; $w ++) {
+ 		echo "<td> i= ".$i." w = ".$w."</td>";
  	}
+ 	echo '</tr>';
  }
 
  ?>
+</table>
+
+<?php
+  $alphabet = array("", "A", "B", "C", "D");
+
+  for( $i = 0; $i <= 4; $i++) {
+  	echo $i."->".$alphabet[$i]."<br>";
+  }
+
+
+?>
+
+<table border="1">
+<?php
+  $alphabet = array("","A","B","C");
+
+  for( $i = 1; $i <= 3; $i++) {
+  	echo '<tr>';
+  	for( $w = 1; $w <= 3; $w++) {
+  		echo "<td>".$alphabet[$i].$w."</td>";
+  	}
+  	echo '</tr>';
+  }
+
+
+?>
 </table>
 </body>
 </html>
